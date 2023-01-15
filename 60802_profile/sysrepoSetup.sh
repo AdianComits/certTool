a0c052d61e74ba43856e02a3728d1f435c694b4e
AdianComits <81190272+AdianComits@users.noreply.github.com>
2023-01-15 23:02:41 +0100

    NACM rules
    NACM rules implemented

--- 60802_profile/sysrepoSetup.sh
+++ 60802_profile/sysrepoSetup.sh
@@ -1,5 +1,5 @@
  #!/bin/sh
 
-sudo sysrepocfg --edit=/home/adian/workspace_60802/netopeer2/example_configuration/tls_keystore.xml  -m ietf-keystore
-sudo sysrepocfg --edit=/home/adian/workspace_60802/netopeer2/example_configuration/tls_truststore.xml -m ietf-truststore
-sudo sysrepocfg --edit=/home/adian/workspace_60802/netopeer2/example_configuration/tls_listen.xml -m ietf-netconf-server
+sudo sysrepocfg --edit=/home/adian/workspace/netopeer2/example_configuration/tls_keystore.xml  -m ietf-keystore
+sudo sysrepocfg --edit=/home/adian/workspace/netopeer2/example_configuration/tls_truststore.xml -m ietf-truststore
+sudo sysrepocfg --edit=/home/adian/workspace/netopeer2/example_configuration/tls_listen.xml -m ietf-netconf-server
